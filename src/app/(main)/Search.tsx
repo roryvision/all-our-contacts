4f66bf4a32e0e31d3a0abcd00755d105c05e5713
roryvision <104661206+roryvision@users.noreply.github.com>
2024-06-30 19:57:49 -0400

    Implement shadcn and lucide

--- src/app/(main)/Search.tsx
+++ src/app/(main)/Search.tsx
@@ -1,89 +1,91 @@
-'use client'
-
 import { signIn } from 'next-auth/react';
-import { FC, FormEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { ToastContainer } from 'react-toastify';
 import { showToastError } from '@/utils/toast';
-import Button from '@/components/Button';
-import InputField from '@/components/InputField';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 interface SearchProps {
   groupId: string
 }
 
-interface FormData {
-  id: string;
-  password: string;
-}
+const formSchema = z.object({
+  groupId: z.string().length(8, {
+    message: "Please enter an 8-digit ID",
+  }),
+  password: z.string().min(1, {
+    message: "Password is required"
+  })
+})
 
-const Search: FC<SearchProps> = ({ groupId }) => {
+const Search = ({ groupId }: SearchProps) => {
   const router = useRouter();
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
-
-  if (groupId === 'search') {
-    groupId = ''
-  }
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-
-    const formId: string | null = formData.get('id') as string;
-    const formPassword: string | null = formData.get('password') as string;
-
-    if (!formId || !formPassword) {
-      setErrors({
-        formId: !formId ? 'Please enter an 8-digit ID' : '',
-        formPassword: !formPassword ? 'Please enter a password' : '',
-      })
-      return;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      groupId: groupId === 'search' ? '' : groupId,
+      password: ''
     }
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const { groupId, password } = values;
 
     try {
       const response = await signIn('credentials', {
-        id: formId,
-        password: formPassword,
-        callbackUrl: `/${formData.get('id')}/add`,
+        id: groupId,
+        password: password,
+        callbackUrl: `/${groupId}/add`,
         redirect: false,
       })
 
       if (response && response.status === 401) {
         showToastError("Group could not be found with the provided credentials.");
       } else if (response && response.ok) {
-        groupId = formId;
         router.push(`/${groupId}/add`);
       }
     } catch(error) {
       showToastError("An unexpected error occurred. Please try again later.");
-    }
+    }    
   }
 
   return (
-    <div>
-      <ToastContainer />
-      <form onSubmit={handleSubmit}>
-        <h2 className='text-3xl font-normal mb-4 w-40'>Let's find your group</h2>
-        <InputField
-          label='Group ID'
-          name='id'
-          type='text'
-          placeholder='8-Digit ID'
-          defaultValue={groupId !== 'search' ? groupId : undefined}
-          maxLength={8}
-          hasError={!!errors.formId} />
-        {errors.formId && <p className='text-xs text-red-500 mb-2'>{errors.formId}</p>}
-        <InputField
-          label='Password'
-          name='password'
-          type='password'
-          placeholder='***********'
-          hasError={!!errors.formPassword} />
-        {errors.formPassword && <p className='text-xs text-red-500 mb-2'>{errors.formPassword}</p>}
-        <Button className='py-2 mt-4' isLoading={false}>Search</Button>
-        <a href='/create' className='font-jetbrains text-xs underline text-center block mt-2 opacity-80'>Create a group</a>
-      </form>
-    </div>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-2'>
+          <FormField
+            control={form.control}
+            name='groupId'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Group ID</FormLabel>
+                <FormControl>
+                  <Input placeholder='8 digit ID' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder='********' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type='submit'>Search</Button>
+        </form>
+      </Form>
+    </>
   )
 }
 
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,80 +1,24 @@
 'use client'
 
-import { useState, MouseEvent } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import Button from '@/components/Button';
-import Message from '../../../public/ui/message.svg';
-import CloseIcon from '../../../public/ui/close.svg';
-import SearchIcon from '../../../public/ui/search.svg';
-import AddIcon from '../../../public/ui/create.svg';
-import DownloadIcon from '../../../public/ui/download.svg';
+import { Button } from '@/components/ui/button';
 import Search from './Search';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 
 export default function Home() {
-  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
-
-  const handleScroll = (e: MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-
-    const targetId = e.currentTarget.href.replace(/.*\#/, '');
-    const elem = document.getElementById(targetId);
-    elem?.scrollIntoView({
-      behavior: 'smooth',
-    })
-  }
-
   return (
     <>
-      <div className='relative z-10 h-screen w-80 block m-auto mt-48'>
-        <div className='absolute top-0 right-0 z-0'>
-          <Image src={Message} alt='can everyone send their names pls' />
-        </div>
-        <div className='w-full'>
-          <div className='w-fit block m-auto pt-28'>
-            <h1 className='text-5xl'>All Our</h1>
-            <h1 className='text-5xl font-bold text-sapphire'>Contacts</h1>
-            <p className='text-xl my-4'>Know those unknown numbers <br /> with one <span className='text-sapphire'>effortless</span> download</p>
-          </div>
-          <div className='w-60 block m-auto my-16'>
-            <Button className='py-4' isLoading={false} onClick={() => setModalIsOpen(true)}>Get Started</Button>
-            <Link onClick = {handleScroll} href='#pitch'><p className='font-jetbrains text-center text-xs underline mt-3'>How does it work?</p></Link>
-          </div>
-        </div>
-      </div>
-      <div id='pitch'>
-        <div>
-          <Image
-            src={SearchIcon}
-            alt='Close'
-            onClick={() => setModalIsOpen(false)} />
-          <h2>Find your group</h2>
-        </div>
-        <div>
-          <Image
-            src={AddIcon}
-            alt='Close'
-            onClick={() => setModalIsOpen(false)} />
-          <h2>Add your contact</h2>
-        </div>
-        <div>
-          <Image
-            src={DownloadIcon}
-            alt='Close'
-            onClick={() => setModalIsOpen(false)} />
-          <h2>Download everybody</h2>
-        </div>
-      </div>
-      <div className={`fixed top-0 left-0 z-30 w-full h-full bg-black/40 backdrop-blur-sm ${modalIsOpen ? 'block' : 'hidden'}`}>
-        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-8 w-72 rounded-lg'>
-          <Image 
-            src={CloseIcon} 
-            alt='Close' 
-            onClick={() => setModalIsOpen(false)}
-            className='float-right hover:cursor-pointer' /> <br />
-          <Search groupId={''} />
-        </div>
-      </div>
+      <div>all our contacts</div>
+      <Dialog>
+        <DialogTrigger>
+          <Button>Get started</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Let's find your group</DialogTitle>
+          </DialogHeader>
+          <Search groupId='' />
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
